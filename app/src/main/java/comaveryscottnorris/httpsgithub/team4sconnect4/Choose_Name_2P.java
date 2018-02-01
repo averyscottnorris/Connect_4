3153a102aaa794f00428f03a458dde0d6cfa3cb1
averyscottnorris <Artem1s2011>
2018-01-31 18:10:04 -0800

    Removed floating action button from choose name pages.

--- app/src/main/java/comaveryscottnorris/httpsgithub/team4sconnect4/Choose_Name_2P.java
+++ app/src/main/java/comaveryscottnorris/httpsgithub/team4sconnect4/Choose_Name_2P.java
@@ -51,16 +51,6 @@ public class Choose_Name_2P extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
