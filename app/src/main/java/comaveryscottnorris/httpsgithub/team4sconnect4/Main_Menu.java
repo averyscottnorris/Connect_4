712d3d68dfa7286b591379a12f987aa71ebef1ed
averyscottnorris <Artem1s2011>
2018-02-01 10:46:53 -0800

    Removed main tag from gameActivity

--- app/src/main/java/comaveryscottnorris/httpsgithub/team4sconnect4/Main_Menu.java
+++ app/src/main/java/comaveryscottnorris/httpsgithub/team4sconnect4/Main_Menu.java
@@ -3,12 +3,12 @@ package comaveryscottnorris.httpsgithub.team4sconnect4;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Main_Menu extends AppCompatActivity {
 
@@ -18,6 +18,16 @@ public class Main_Menu extends AppCompatActivity {
         setContentView(R.layout.main_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button b = findViewById(R.id.testbutton);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Main_Menu.this, Choose_Name_2P.class);
+                startActivity(myIntent);
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
